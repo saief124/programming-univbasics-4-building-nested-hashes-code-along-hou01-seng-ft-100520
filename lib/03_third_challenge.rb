9c72f7131414f13a80caacb463e8dddc8a17ff32
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-16 00:15:09 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,25 +2,25 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :Name=>"Lord Montague", :Age=>"53"#your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
+        :Name=>"Lady Montague", :Age=>"54"#your key/value pairs here
         },
       :hero => {
-        #your key/value pairs here
+        :Name=>"Romeo", :Age=>"15", :Status=>"Alive"#your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :Name=>"Lord Capulet", :Age=>"50"#your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
+        :Name=>"Lady Capulet", :Age=>"51"#your key/value pairs here
         },
       :heroine => {
-        #your key/value pairs here
+        :Name=>"Juliet", :Age=>"15"#your key/value pairs here
         },
       :heroine_friends => []
    }
